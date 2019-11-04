efa8e1bd6dc1c529cc67e0aee01e1be65efbbbab
Cogent Health <apple@Apples-iMac-2.local>
2019-11-04 14:11:36 +0545

    react bootstrap customize

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
+import { Button } from 'react-bootstrap';
+import './scss/custom.scss';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <Button variant="outline-primary">Primary</Button>
+    <Button variant="outline-secondary">Secondary</Button>
+    <Button variant="outline-success">Success</Button>
+    <Button variant="outline-warning">Warning</Button>
+    <Button variant="outline-danger">Danger</Button>
+    <Button variant="outline-info">Info</Button>
+    <Button variant="outline-light">Light</Button>
+    <Button variant="outline-dark">Dark</Button>
     </div>
   );
 }
