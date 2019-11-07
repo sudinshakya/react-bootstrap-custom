cb4fd093bed8891eb183e5f8c38e1d457ddac8ab
Cogent Health <apple@Apples-iMac-2.local>
2019-11-07 09:32:15 +0545

    ejected file

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 
-import './App.css';
-import { Button } from 'react-bootstrap';
-import './scss/custom.scss';
+import './App.scss';
+import { Button,Breadcrumb,Dropdown } from 'react-bootstrap';
+import 'bootstrap/scss/bootstrap.scss';
+ import './scss/custom.scss';
+
 
 function App() {
   return (
+    <>
     <div className="App">
      <Button variant="outline-primary">Primary</Button>
     <Button variant="outline-secondary">Secondary</Button>
@@ -16,6 +19,29 @@ function App() {
     <Button variant="outline-light">Light</Button>
     <Button variant="outline-dark">Dark</Button>
     </div>
+    <div>
+
+<Breadcrumb className="breadcrumb1"> 
+  <Breadcrumb.Item href="#">Home</Breadcrumb.Item>
+  <Breadcrumb.Item href="https://getbootstrap.com/docs/4.0/components/breadcrumb/">
+    Library
+  </Breadcrumb.Item>
+  <Breadcrumb.Item active>Data</Breadcrumb.Item>
+</Breadcrumb>
+
+<Dropdown>
+  <Dropdown.Toggle variant="success" id="dropdown-basic">
+    Dropdown Button
+  </Dropdown.Toggle>
+
+  <Dropdown.Menu>
+    <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
+    <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
+    <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+  </Dropdown.Menu>
+</Dropdown>
+    </div>
+    </>
   );
 }
 
